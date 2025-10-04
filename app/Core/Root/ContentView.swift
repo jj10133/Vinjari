8513af0d744bd46f3b34446c6ba597da84170c08
JJ <warmachinesocial@skiff.com>
2025-10-04 17:18:45 -0500

    Fix - Added progress

--- app/Core/Root/ContentView.swift
+++ app/Core/Root/ContentView.swift
@@ -21,17 +21,17 @@ struct ContentView: View {
                         if let url = URL(string: "hyper://0143faffb6927994c414ccb46f6b10e0ecc5e4dfe0301207a4b96239897eac4c") {
                             page.load(URLRequest(url: url))
                         }
-                    }
+                }
             } else {
-                ProgressView("Loading...")
+                ProgressView("Loading...", value: page?.estimatedProgress)
                     .onAppear {
                         let scheme = URLScheme("hyper")!
                         let handler = HyperResourceSchemeHandler(ipc: ipcViewModel.ipc)
                         
                         var configuration = WebPage.Configuration()
                         configuration.urlSchemeHandlers[scheme] = handler
-                        
                         page = WebPage(configuration: configuration)
+                        
                     }
             }
         }
